9ace55e9f4b54ec3303669f57e0e6c18fad6d9c3
Fernanda <fernanda.marana44@gmail.com>
2019-04-17 12:12:47 +0200

    Deleted useless files and modified noisyifneuron into a function

--- Exercise 1/noisyifneuron.m
+++ Exercise 1/noisyifneuron.m
@@ -1,11 +1,12 @@
+function plotNeuron = noisyifneuron(tau,theta,Rin)
 nstep = 100; % Number of timesteps to integrate over
 Inoise = 0.1;
 I0 = 1+Inoise*randn(1,nstep); % Input current in nA
 dt = 1; % time step in ms
-tau = 10; % membrane time constant in ms
-theta = 4; % threshold in mV
+%tau = 10; % membrane time constant in ms
+%theta = 4; % threshold in mV
 v = zeros(1,nstep);
-Rin = 5; % Input resistance in MOhm
+%Rin = 5; % Input resistance in MOhm
 tspike = [];
 t = (1:nstep)*dt;
 for n=2:nstep
@@ -16,3 +17,4 @@ tspike = [ tspike t(n) ];
 end
 end
 plot(t,v)
+end
